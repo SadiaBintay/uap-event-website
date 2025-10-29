770c783acf757d20b8ad691f0af68cdeb939002a
sadiaBintay <23101155@uap-bd.edu>
2025-10-30 00:17:48 +0600

    notification system

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,10 +3,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # ← Add this line
     path('accounts/', include('accounts.urls')),
+    path('notifications/', include('notifications.urls')),
 ]
 
 if settings.DEBUG:
